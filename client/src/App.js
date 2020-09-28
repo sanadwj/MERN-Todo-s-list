1c78ddd93870adbce93012715b0f3e4eaa5e07ce
Sanad <s_wj_88@hotmail.com>
2020-09-28 13:52:11 +0300

    Add modal for add item

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import AppNavbar from './components/AppNavbar'
 import ShoppingList from './components/ShoppingList'
+import ItemModal from './components/itemModal'
+import { Container } from  'reactstrap'
 import { Provider } from 'react-redux'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import itemModal from './components/itemModal';
 
 function App() {
   return (
     <Provider store={store}>
     <div className="App">
       <AppNavbar />
+      <Container>
+      <ItemModal/>
       <ShoppingList/>
+      </Container>
+      
   </div>
     </Provider>
     
